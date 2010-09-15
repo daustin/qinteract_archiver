c1785f47890e1db8fdf1322c68d53a3922c143e0
David Austin <daustin@meath.itmat.upenn.edu>
2010-09-15 11:27:15 -0400

    Modified iarchive_readme_updater to bypass client.thor

--- iarchive_readme_updater.rb
+++ iarchive_readme_updater.rb
@@ -12,6 +12,8 @@
 #######
 
 MD5CMD = 'md5 -q' # needs to output only md5 hash, or a wsv with the hash as the first value 
+PZIPCMD = '7za'
+
 disk_id = File.basename(Dir.pwd)
 
 unless File.exist? ARGV[0]
@@ -21,32 +23,75 @@ unless File.exist? ARGV[0]
   
 end
 
-folder_name = ARGV[0]
+folder_name = ARGV[0].strip.chomp('/')
+
+puts "Updating archive #{folder_name}..."
 
 # extract readme, alter and resave within
 Dir.chdir "#{folder_name}" do
-  puts "Updating README MD5"  
 
   # now this is where we extract the readme, alter it, and then save it to iarchive
 
   puts "Updating disk id"
-  system "thor client:update_readme #{folder_name}.7z.001 archive_disk_id #{disk_id}"
+  
+  # delete existing ./README.json
+  
+  system "rm -rf README.json"
+  
+  # fetch project_xxx/README.json from 7zip to cwd
 
-  puts "Uploading to iarchive..."
-  cout = `curl -v -F "readme_file=@README.json" -u iarchive:iarchive http://bioinf.itmat.upenn.edu/iarchive/create 2>&1`
+  system "#{PZIPCMD} x -r #{folder_name}.7z.001 #{folder_name}/README.json"  
+  
+  # import into memory
+  
+  json_hash = JSON.parse(File.read("#{folder_name}/README.json"))
+  
+  # add disk ID, save to ./ReADMEjson
+  
+  json_hash['archive_disk_id'] = disk_id
+  
+  fout = File.open("#{folder_name}/README.json", "w+")
+  fout.write json_hash.to_json
+  fout.close
+    
+  # post new README to iarchive
   new_id = 0
-  cout.each_line do |el|
-    if el =~ /archive\/show\/(\d+)/
-      new_id = $1 
+  Dir.chdir(folder_name) do
+    
+    puts "Uploading to iarchive..."
+    cout = `curl -v -F "readme_file=@README.json" -u iarchive:iarchive http://bioinf.itmat.upenn.edu/iarchive/create 2>&1`
+    cout.each_line do |el|
+      new_id = $1 if el =~ /archive\/show\/(\d+)/
     end
-  end
 
-  if new_id == 0
-    puts "ERROR: Could not fetch new archive id from iarchive."
-  else
-      system "thor client:update_readme #{folder_name}.7z.001 iarchive_id #{new_id}"
-      `curl -v -F "readme_file=@README.json" -u iarchive:iarchive http://bioinf.itmat.upenn.edu/iarchive/update/#{new_id}`
+    exit "ERROR: Could not fetch new archive id from iarchive." if new_id == 0
+    
   end
+  
+  # add iarchive ID to existing JSON in memory
+  puts "Updating iarchive id..."
+  
+  json_hash['iarchive_id'] = new_id
+
+  fout = File.open("#{folder_name}/README.json", "w+")
+  fout.write json_hash.to_json
+  fout.close 
+
+  Dir.chdir(folder_name) do
+    
+    puts "Uploading to iarchive..."
+    `curl -v -F "readme_file=@README.json" -u iarchive:iarchive http://bioinf.itmat.upenn.edu/iarchive/update/#{new_id}`
+    
+  end
+  
+  # update README.json reference in 7z file
+    
+  system "#{PZIPCMD} u -mx6 -mmt #{folder_name}.7z.001 #{folder_name}/README.json"
+  
+  # cp readme to cwd and cleanup
+  
+  system "cp -v #{folder_name}/README.json ."
+  system "rm -rf #{folder_name}/"
 
 end
 
